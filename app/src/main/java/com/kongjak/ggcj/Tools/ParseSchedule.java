106dbe4e13d1cc499225d6e4f8327b0a168ae193
kongwoojin <kongwoojin03@gmail.com>
2020-03-13 00:36:26 +0900

    학사일정 데이터를 NEIS Open API로 파싱하도록 변경

--- app/src/main/java/com/kongjak/ggcj/Tools/ParseSchedule.java
+++ app/src/main/java/com/kongjak/ggcj/Tools/ParseSchedule.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.AsyncTask;
 
 import com.kongjak.ggcj.R;
@@ -14,6 +15,10 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import static android.content.Context.MODE_PRIVATE;
 
@@ -51,43 +56,71 @@ public class ParseSchedule {
 
         @Override
         protected Void doInBackground(String... params) {
-            //백그라운드 작업이 진행되는 곳.
-            String school_code = context.getString(R.string.school_code);
-            String school_type = context.getString(R.string.school_type);
-            int day = 0;
-
             String year = params[0];
             String month = params[1];
 
-            Document doc;
-            String schedule_url = String.format(context.getString(R.string.neis_schedule), "goe", school_code, school_type, year, month);
             try {
-                doc = Jsoup.connect(schedule_url).get();
-                Elements scheduleAll = doc.select("#contents > div:nth-child(2) > table > tbody > tr");
-                for (Element schedulesList : scheduleAll) {
-                    Elements schedules = schedulesList.select("td > div");
-                    for (Element schedule : schedules) {
-                        StringBuilder schedule_txt = new StringBuilder();
-                        Elements scheduleDate = schedule.select("em");
-                        if (scheduleDate.text().isEmpty())
-                            continue;
-                        Elements scheduleA = schedule.select("a");
-                        for (Element scA : scheduleA) {
-                            Elements scheduleTxt = scA.select("strong");
-                            schedule_txt.append(scheduleTxt.text()).append("\n");
+                String date = year + month;
+                Uri.Builder builder = new Uri.Builder();
+                builder.scheme("https")
+                        .authority("open.neis.go.kr")
+                        .appendPath("hub")
+                        .appendPath("SchoolSchedule")
+                        .appendQueryParameter("KEY", context.getString(R.string.api_key))
+                        .appendQueryParameter("ATPT_OFCDC_SC_CODE", context.getString(R.string.api_region_code))
+                        .appendQueryParameter("SD_SCHUL_CODE", context.getString(R.string.api_school_code))
+                        .appendQueryParameter("AA_YMD", date);
+                String schedule_url = builder.build().toString();
+
+                Document doc = Jsoup.connect(schedule_url).get();
+
+                boolean isClassExits = false;
+
+                Elements code = doc.select("* > RESULT >CODE");
+
+                if (code.text().equals("INFO-000")) {
+                    isClassExits = true;
+                }
+
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMM");
+                Date convertedDate = dateFormat.parse(date);
+                Calendar c = Calendar.getInstance();
+                c.setTime(convertedDate);
+
+                int last_day = c.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+                for (int i = 1; i <= last_day; i++) {
+                    String date_pref = year + "-" + month + "-" + CheckDigit.check(String.valueOf(i));
+                    SharedPreferences schedule_sp = context.getSharedPreferences("schedule", MODE_PRIVATE);
+                    SharedPreferences.Editor schedule_editor = schedule_sp.edit();
+                    schedule_editor.putString(date_pref, context.getString(R.string.no_schedule));
+                    schedule_editor.apply();
+                }
+
+                if (isClassExits) {
+                    Elements element = doc.select("SchoolSchedule");
+                    for (Element e : element) {
+                        int count = Integer.parseInt(e.select("list_total_count").get(0).text());
+                        for (int i = 0; i < count; i++) {
+                            String schedule = e.select("EVENT_NM").get(i).text();
+                            String date_parsed = e.select("AA_YMD").get(i).text();
+                            String year_parsed = date_parsed.substring(0, 4);
+                            String month_parsed = date_parsed.substring(4, 6);
+                            String day_parsed = date_parsed.substring(6, 8);
+
+                            String date_pref1 = year_parsed + "-" + month_parsed + "-" + day_parsed;
+
+                            schedule = schedule.replace("<br/>", "\n");
+
+                            SharedPreferences schedule_sp = context.getSharedPreferences("schedule", MODE_PRIVATE);
+                            SharedPreferences.Editor schedule_editor = schedule_sp.edit();
+                            schedule_editor.putString(date_pref1, schedule);
+                            schedule_editor.apply();
+
                         }
-                        day = day + 1;
-                        String Date = year + "-" + month + "-" + CheckDigit.check(String.valueOf(day));
-                        SharedPreferences schedule_sp = context.getSharedPreferences("schedule", MODE_PRIVATE);
-                        SharedPreferences.Editor schedule_editor = schedule_sp.edit();
-                        if (schedule_txt.toString().isEmpty())
-                            schedule_editor.putString(Date, context.getString(R.string.no_schedule));
-                        else
-                            schedule_editor.putString(Date, schedule_txt.toString().trim());
-                        schedule_editor.apply();
                     }
                 }
-            } catch (IOException e) {
+            } catch (IOException | ParseException e) {
                 e.printStackTrace();
             }
             return null;
